c4e2cc6811025cbe19b6291aa95554963683b763
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-08-14 13:19:01 -0700

    added left block

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,23 @@
 <body>
-  <div class="sticky flex justify-center py-3 shadow-md">
+  <div class="sticky flex justify-center py-3 border-b border-gray-300">
     <img src="placeholder.png" alt="placeholder">
   </div>
+  <main class="space-y-16">
+    <div class="w-full">
+      <div class="flex">
+        <div class="relative h-full">
+          <div class="sticky flex">
+            <div class="block w-56 border-r border-b border-gray-300">
+              <div class="flex">
+                <p>Topics</p>
+              </div>
+            </div>
+            <div class="flex-1">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis rerum et nostrum, officiis ea accusantium numquam rem illo, itaque eum velit eos exercitationem illum voluptates similique nobis dignissimos mollitia dolor!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </body>
