88cc2a14a69b6c86b86d5452bbc267f8c6b55a0a
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-08-13 20:41:16 -0700

    added navbar with placeholder

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,5 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<body>
+  <div class="sticky flex justify-center py-3 shadow-md">
+    <img src="placeholder.png" alt="placeholder">
+  </div>
+</body>
