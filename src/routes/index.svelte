3ca3b536b2d0d1ede17ee47a2eb58baf962f5f82
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-08-14 19:49:46 -0700

    menu structuring

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,32 @@
   <div class="sticky flex justify-center py-3 border-b border-gray-300">
     <img src="placeholder.png" alt="placeholder">
   </div>
-  <main class="space-y-16">
+  <main class="">
     <div class="w-full">
       <div class="flex">
         <div class="relative h-full">
           <div class="sticky flex">
             <div class="block w-56 border-r border-b border-gray-300">
-              <div class="flex">
-                <p>Topics</p>
+              <div class="flex p-4">
+                <p class="text-md font-semibold">
+                  Menu
+                </p>
+              </div>
+              <div class="flex px-4 py-2">
+                <nav class="text-sm font-medium">
+                  <ul class="mb-6 space-y-2">
+                    <li>
+                      <p>
+                        1st element
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        2nd element
+                      </p>
+                    </li>
+                  </ul>
+                </nav>
               </div>
             </div>
             <div class="flex-1">
