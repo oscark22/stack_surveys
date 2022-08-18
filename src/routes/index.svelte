e695cdac77add729300141356e3eb55dddd1d731
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-08-17 17:14:33 -0700

    styling and responsivity changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,26 +7,26 @@
       <div class="flex-1">
         <div class="h-full">
           <div class="flex">
-            <div class="hidden lg:block w-80 border-r border-b border-gray-300">
+            <div class="hidden lg:block w-1/6 border-r border-b border-gray-300">
               <div class="h-full">
                 <div class="sticky top-0">
                   <div class="block">
                     <div class="flex px-6 pt-6 pb-4">
-                      <p class="text-md font-semibold">
+                      <p class="text-lg font-bold">
                         Menu
                       </p>
                     </div>
                     <div class="flex px-6 py-2">
-                      <nav class="text-sm font-medium">
+                      <nav class="font-semibold">
                         <ul class="mb-6 space-y-2">
                           <li>
                             <p>
-                              1st element
+                              Random
                             </p>
                           </li>
                           <li>
                             <p>
-                              2nd element
+                              Element
                             </p>
                           </li>
                         </ul>
@@ -47,27 +47,39 @@
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </p>
                   </div>
-                  <div class="space-y-4 py-12">
-                    <div class="text-white rounded-md p-6 lg:p-8 pb-20 bg-gradient-to-br from-blue-700 to-blue-400">
+                  <div class="lg:flex pt-8 lg:pt-12 lg:space-x-6">
+                    <!-- create comp? -->
+                    <div class="text-white lg:w-7/12 rounded-md p-6 lg:p-8 pb-20 lg:pb-40 mb-40 sm:mb-4 bg-gradient-to-br from-emerald-500 to-blue-600">
                       <!-- title card -->
                       <h1 class="text-xl lg:text-2xl font-semibold">
                         About this webpage
                       </h1>
                       <p class="bg-white w-8 h-0.5 my-2">
-                      <!-- body card // create style comp for this? -->
-                      <p class="leading-relaxed pt-6 lg:w-1/2">
+                      <!-- body card -->
+                      <p class="leading-relaxed pt-6">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam possimus necessitatibus.
                       </p>
-                      <p class="leading-relaxed pt-6 lg:w-1/2">
+                      <p class="leading-relaxed pt-6">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed temporibus distinctio sunt odio, itaque nemo excepturi at laudantium quam quia, id commodi soluta. Reprehenderit eius voluptate ea quam deleniti similique.
-                      </p>
-                      <p class="leading-relaxed pt-6 lg:w-1/2">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis magni accusantium itaque hic tempore.
                       </p>
                       <br>
                       <button class="bg-slate-900 py-2 px-8 rounded-md">
                         Overview ->
                       </button>
+                    </div>
+                    <div class="text-white lg:w-5/12 rounded-md p-6 lg:p-8 pb-40 lg:pb-40 mb-4 bg-gradient-to-tr from-blue-600 to-purple-500">
+                      <!-- title card -->
+                      <h1 class="text-xl lg:text-2xl font-semibold">
+                        About licencing
+                      </h1>
+                      <p class="bg-white w-8 h-0.5 my-2">
+                      <!-- body card -->
+                      <p class="leading-relaxed pt-6">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum libero eum fugit laborum fuga. Veniam ex ab eaque sunt culpa maxime nulla repellendus sapiente minus, molestiae, minima hic eveniet vitae.
+                      </p>
+                      <p class="leading-relaxed pt-6">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, totam hic et voluptatem corporis, molestias tempora quae culpa temporibus nisi odit? Sed quas explicabo quibusdam sapiente aut corrupti aperiam ducimus.
+                      </p>
                     </div>
                   </div>
                 </div>
