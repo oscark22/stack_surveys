2a74a0c3345a1d19386f348cf4c4a8f5a6d598cc
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-10-03 14:03:13 -0700

    new comment comp

---
+++ src/lib/comment.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col mb-5">
+  <p class="text-sm text-gray-600 mb-1">user | 2 min ago</p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates facere impedit perferendis ab tenetur, provident ipsum mollitia! Recusandae dolorum, unde rerum qui quod perferendis quam sapiente? Eius similique nobis obcaecati.
+  </p>
+</div>
--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import Title from '$lib/title.svelte';
+  import Comment from '$lib/comment.svelte';
 </script>
 
 <Title 
   title="Comments"
   subtitle="This is a comments section. Feel free to share your thoughts!"
 />
+<div>
+  <Comment />
+</div>
