280a6ee3416aefb554a4dd8af45e2a486244a48c
oscark22 <98506040+oscark22@users.noreply.github.com>
2022-11-27 17:09:45 -0800

    logic for creating datasets

--- src/lib/charts/test.svelte
+++ src/lib/charts/test.svelte
@@ -1,52 +1,72 @@
 <script lang="ts">
 	import Chart from 'chart.js/auto';
+	import type { List } from 'postcss/lib/list';
+	import list from 'postcss/lib/list';
   import { onMount } from 'svelte';
+	import { get } from 'svelte/store';
 
   let canvas: HTMLCanvasElement;
   
-  const labels = [1,2,3,4,5,6,7];
-  const chartData = {
-    labels: labels,
-    datasets: [{
-      label: 'Donuts',
-      data: [65, 59, 80, 81, 56, 55, 40],
-      borderColor: 'rgb(75, 192, 192, 0.6)',
-      pointBackgroundColor: 'rgb(75, 192, 192)',
-      backgroundColor: 'rgb(75, 192, 192, 0.2)',
-      fill: true,
-    }],
-  };
-  
-  onMount(() => {
+  onMount(async () => {
+    const chartData = await (await fetch('http://127.0.0.1:8000/loved_languages/10')).json()
+    const labels = Object.keys(chartData)
+
+    console.log(chartData)
+
+    interface Responses {
+      num_responses: string,
+      total_responses: string    
+    }
+
+    let occurrences = Object(), index = 0
+    for (const label of labels) {
+      let place = 0
+      for (const [language, responses] of Object.entries(chartData[label]["data"])) {
+        let response = responses as Responses
+
+        place += 1
+        if (occurrences.hasOwnProperty(language)) {
+          occurrences[language].push([index, place, response["num_responses"], response["total_responses"]])
+        } else {
+          occurrences[language] = [[index, place, response["num_responses"], response["total_responses"]]]
+        }
+      }
+      index += 1
+    }
+    console.log(occurrences)
+
+    let datasets = Array()
+    // for (const [language, list] of Object.entries(occurrences)) {
+    //   const data = list as Array<String | Number>
+
+    //   datasets.push({
+    //     label: language,
+    //     data: data[1],
+    //   })
+    // }
+
+    const data = {
+      labels: labels,
+      datasets: datasets
+    }
+
+    const options = {
+      elements: {
+        point: {
+          radius: 4,
+        },
+        line: {
+          tension: 0.25,
+        },
+      },
+    }
+
     new Chart(canvas, {
       type: 'line',
-      data: chartData,
-      options: {
-        elements: {
-          point: {
-            radius: 4,
-          },
-          line: {
-            tension: 0.25,
-          },
-        },
-      }
+      data: data,
+      options: options
     });
   });
-
-  // testing
-  async function example() {
-    try {
-      const promise = await fetch('http://127.0.0.1:8000/loved_languages', {
-        method: 'GET'
-      })
-      const data = await promise.json()
-      console.log
-      return data
-    } catch (e) {
-      console.log(e)
-    }
-  }
 
   export let height: number;
 </script>
@@ -55,5 +75,4 @@
   <p>Hello Fallback World</p>
 </canvas>
 
-<button on:click={example}>Adlkfjalfjslkan</button>
 
