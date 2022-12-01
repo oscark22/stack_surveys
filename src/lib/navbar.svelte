8d7137523ab01041e4ddc57a39edc8fb91c57c0c
oscark22 <98506040+oscark22@users.noreply.github.com>
2022-11-30 17:17:42 -0800

    navbar bug fixed

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,7 +1,7 @@
 <div class="hidden lg:block w-1/6 border-r border-b border-gray-300">
   <div class="h-full">
     <div class="sticky top-0">
-      <div class="block">
+      <div class="block h-screen">
         <div class="flex-1">
           <div class="py-3 bg-slate-900">
             <p class="text-center text-white text-lg font-semibold">
